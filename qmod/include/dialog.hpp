a6e9ed1ba6267214d644bdb53602a2ba02174669
Dan Rouse <dan@rouse.house>
2021-09-03 01:41:41 -0400

    qmod: SSL, make web request non-blocking

--- qmod/include/dialog.hpp
+++ qmod/include/dialog.hpp
@@ -1,10 +1,16 @@
 #pragma once
 
+#include <thread>
+#include <future>
+
 #include "questui/shared/BeatSaberUI.hpp"
+#include "questui/shared/CustomTypes/Components/Backgroundable.hpp"
+#include "custom-types/shared/coroutine.hpp"
 #include "libcurl/shared/curl.h"
-#include <sys/stat.h>
 
 #include "UnityEngine/Application.hpp"
+#include "UnityEngine/WaitForEndOfFrame.hpp"
+#include "System/Collections/IEnumerator.hpp"
 
 #include "logger.hpp"
 
@@ -14,3 +20,12 @@ void uploadCrashLog(
     std::function<void(std::string responseText)> onSuccess,
     std::function<void(const char* errorMessage)> onFailure
 );
+
+template<typename T>
+custom_types::Helpers::Coroutine waitForFuture(std::future<T> fut, std::function<void(T)> onComplete) {
+    while (fut.wait_for(std::chrono::seconds(0)) != std::future_status::ready) {
+        co_yield reinterpret_cast<System::Collections::IEnumerator*>(CRASH_UNLESS(UnityEngine::WaitForEndOfFrame::New_ctor()));
+    }
+    onComplete(fut.get());
+    co_return;
+}
--- qmod/src/dialog.cpp
+++ qmod/src/dialog.cpp
@@ -1,21 +1,20 @@
 #include "dialog.hpp"
-#include <thread>
 
 // TODO: Is there a way to inject the host URI at build-time?
-const char* TOMBSTONE_POST_URL = "http://mods.quest/upload-tombstone";
+const char* TOMBSTONE_POST_URL = "https://mods.quest/upload-tombstone";
 // const char* TOMBSTONE_POST_URL = "http://192.168.1.16:3000/upload-tombstone";
 
-static size_t writeCallback(void *contents, size_t size, size_t nmemb, void *userp) {
-    ((std::string*)userp)->append((char*)contents, size * nmemb);
+static size_t cURLWriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+    ((std::string*)userp)->append((char*)contents, size* nmemb);
     return size * nmemb;
 }
 
-void uploadCrashLog(
-    const char* tombstonePath,
-    // TODO: these types don't line up and it feels bad
-    std::function<void(std::string responseText)> onSuccess,
-    std::function<void(const char* errorMessage)> onFailure
-) {
+struct cURLResult {
+    bool success;
+    std::string text;
+};
+
+cURLResult uploadCrashLog(const char* tombstonePath) {
     std::string responseText;
 
     auto curl = curl_easy_init();
@@ -40,12 +39,17 @@ void uploadCrashLog(
     curl_mime_name(field, "os");
     curl_mime_data(field, to_utf8(csstrtostr(operatingSystem)).c_str(), CURL_ZERO_TERMINATED);
 
+    // TODO? Oh no this is so bad...
+    // Trying to save a CA cert at runtime was failing, though,
+    // and Android system CA certs didn't appear to work.
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, false);
+
     struct curl_slist* headers = NULL;
     headers = curl_slist_append(headers, "Expect:");
     curl_easy_setopt(curl, CURLOPT_URL, TOMBSTONE_POST_URL);
     curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
     curl_easy_setopt(curl, CURLOPT_MIMEPOST, form);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, cURLWriteCallback);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseText);
 
     CURLcode res = curl_easy_perform(curl);
@@ -54,64 +58,81 @@ void uploadCrashLog(
     curl_slist_free_all(headers);
 
     if (res != CURLE_OK) {
-        onFailure(curl_easy_strerror(res));
+        return {false, std::string(curl_easy_strerror(res))};
     } else {
-        onSuccess(responseText);
+        return {true, responseText};
     }
 }
 
+// Q: if these buttons are defined inside showCrashReportDialog,
+//    then the game crashes when trying to call methods on them
+//    in the callback lambda
+UnityEngine::UI::Button* confirmButton;
+UnityEngine::UI::Button* cancelButton;
+bool isUploadInProgress;
+
 void showCrashReportDialog(const char* tombstonePath) {
-    // TODO: Generally improve visuals here
-    auto container = QuestUI::BeatSaberUI::CreateFloatingScreen(
-        UnityEngine::Vector2(3.0f, 6.0f),
-        UnityEngine::Vector3(0.0f, 1.5f, 2.5f),
-        UnityEngine::Vector3(0.0f, 0.0f, 0.0f),
-        0.0f,
-        true
-    );
-    // TODO: Background isn't working :(
-    // container
-    //     ->AddComponent<QuestUI::Backgroundable*>()
-    //     ->ApplyBackgroundWithAlpha(il2cpp_utils::createcsstr("round-rect-panel"), 0.96f);
+    auto container = QuestUI::BeatSaberUI::CreateCanvas();
+    container->get_transform()->set_position(UnityEngine::Vector3(0.0f, 1.5f, 2.5f));
+    container->GetComponent<UnityEngine::RectTransform*>()->set_sizeDelta(UnityEngine::Vector2(74.0f, 30.0f));
+    auto background = container->AddComponent<QuestUI::Backgroundable*>();
+    static auto backgroundName = il2cpp_utils::createcsstr("round-rect-panel", il2cpp_utils::StringType::Manual);
+    background->ApplyBackgroundWithAlpha(il2cpp_utils::createcsstr("round-rect-panel"), 0.95f);
 
-    auto text = QuestUI::BeatSaberUI::CreateText(
+    auto dialogText = QuestUI::BeatSaberUI::CreateText(
         container->get_transform(),
-        "It looks like your game crashed recently!\nWould you like to upload an error report\nto help diagnose and fix this issue?",
-        UnityEngine::Vector2(0.0f, 0.0f)
+        "<line-height=80%>It looks like your game crashed recently!\n"
+        "Would you like to upload an error report\nto help diagnose and fix this issue?",
+        UnityEngine::Vector2(-2.0f, 7.0f)
     );
-    // text->set_lineSpacing(0.1f);
-
-    UnityEngine::UI::Button* cancelButton;
-    UnityEngine::UI::Button* confirmButton = QuestUI::BeatSaberUI::CreateUIButton(
+    
+    confirmButton = QuestUI::BeatSaberUI::CreateUIButton(
         container->get_transform(),
         "Send Error Report",
-        UnityEngine::Vector2(-16.5f, -18.0f),
-        UnityEngine::Vector2(32.0f, 10.0f),
+        "PlayButton",
+        UnityEngine::Vector2(-15.5f, -8.0f),
+        UnityEngine::Vector2(34.0f, 10.0f),
         [=]() {
-            // TODO: Putting this in a thread crashes here
-            // std::thread(
-            uploadCrashLog(
-                tombstonePath,
-                [=](std::string responseText) {
-                    getLogger().info("Tombstone uploaded successfully");
-                    text->set_text(il2cpp_utils::createcsstr("Success! Your log can be found at:\nhttp://mods.quest/tombstones/" + responseText));
-                    // TODO: Changing cancelButton text or removing confirmButton both crash here
-                },
-                [=](const char* errorMessage) {
-                    getLogger().info("Web request failed with error: %s", errorMessage);
-                    text->set_text(il2cpp_utils::createcsstr("Error uploading log!\n" + std::string(errorMessage)));
-                }
-            );           
+            // only allow a single upload
+            if (isUploadInProgress) return;
+            isUploadInProgress = true;
+
+            // start web request on another thread
+            auto fut = std::async<function_ptr_t<cURLResult, const char*>>(
+                std::launch::async,
+                &uploadCrashLog,
+                tombstonePath
+            );
+            QuestUI::BeatSaberUI::SetButtonText(confirmButton, "Uploading...");
+
+            // put coroutine on a random MB to wait for web request to finish
+            background->StartCoroutine(reinterpret_cast<System::Collections::IEnumerator*>(custom_types::Helpers::CoroutineHelper::New(
+                waitForFuture<cURLResult>(std::move(fut), [=](cURLResult res) {
+                    isUploadInProgress = false;
+                    if (res.success) {
+                        getLogger().info("Tombstone uploaded successfully");
+                        dialogText->set_text(il2cpp_utils::createcsstr(
+                            "<line-height=120%>Success! Your log can be found at:\n"
+                            "<size=80%><align=\"center\">https://mods.quest/tombstones/" + res.text));
+                        confirmButton->get_gameObject()->set_active(false);
+                        auto cancelButtonTransform = cancelButton->GetComponent<UnityEngine::RectTransform*>();
+                        cancelButtonTransform->set_sizeDelta(UnityEngine::Vector2(64.0f, 10.0f));
+                        cancelButtonTransform->set_anchoredPosition(UnityEngine::Vector2(0.0f, -8.0f));
+                        QuestUI::BeatSaberUI::SetButtonText(cancelButton, "Close");
+                    } else {
+                        getLogger().info("Web request failed with error: %s", res.text.c_str());
+                        dialogText->set_text(il2cpp_utils::createcsstr("Error uploading log!\n" + res.text));
+                    }
+                })
+            )));
         }
     );
 
     cancelButton = QuestUI::BeatSaberUI::CreateUIButton(
         container->get_transform(),
         "Don't Send",
-        UnityEngine::Vector2(16.5f, -18.0f),
-        UnityEngine::Vector2(32.0f, 10.0f),
-        [=]() {
-            UnityEngine::Object::Destroy(container);
-        }
+        UnityEngine::Vector2(17.5f, -8.0f),
+        UnityEngine::Vector2(30.0f, 10.0f),
+        [container]() { UnityEngine::Object::Destroy(container); }
     );
 }
