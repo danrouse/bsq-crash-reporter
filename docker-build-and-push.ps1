4eeac83b955d627078cc946077928514fd511484
Dan Rouse <dan@rouse.house>
2021-09-09 21:00:51 -0400

    docker build: compile typescript before build

--- docker-build-and-push.ps1
+++ docker-build-and-push.ps1
@@ -1,4 +1,5 @@
 $gcrUrl = "gcr.io/bsq-crash-reporter/bsq-crash-reporter"
+& cd server; npm run build; cd ..
 & docker build --tag bsq-crash-reporter:latest --tag $gcrUrl .
 & docker push $gcrUrl
 & gcloud compute instances update-container bsq-crash-reporter --container-image $gcrUrl
